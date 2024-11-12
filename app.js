23d3c9b742c73f630b839147793da1d0a3bcbbc2
MuhammadZeshanLatif <zeeshanmuhammad42201@gmail.com>
2024-11-12 17:56:14 +0500

    Update

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-// Initialize Lenis
-const lenis = new Lenis();
+// // Initialize Lenis
+// const lenis = new Lenis();
 
-// Listen for the scroll event and log the event data
-lenis.on('scroll', (e) => {
-  console.log(e);
-});
+// // Listen for the scroll event and log the event data
+// lenis.on('scroll', (e) => {
+//   console.log(e);
+// });
 
-// Use requestAnimationFrame to continuously update the scroll
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-requestAnimationFrame(raf);
+// // Use requestAnimationFrame to continuously update the scroll
+// function raf(time) {
+//   lenis.raf(time);
+//   requestAnimationFrame(raf);
+// }
+// requestAnimationFrame(raf);
 
 // Initialize GSAP Box Reveal Animation
 window.addEventListener('load', () => {
